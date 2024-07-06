df6accfde6340cf93fdda090accdcc7cf2900f4a
YO-RO <45329703+YO-RO@users.noreply.github.com>
2024-07-07 01:14:52 +0900

    refactor: ファイル名を変更
    処理が多くなったのでparserではなくanalyzerファイルに分割する

---
+++ parser/analyzer.go
@@ -0,0 +1,184 @@
+package parser
+
+import (
+	"errors"
+	"fmt"
+	"regexp"
+	"strconv"
+)
+
+type TokenType int
+
+const (
+	String TokenType = iota + 1
+	Int
+	Float
+	Bool
+
+	LeftSquareBracket
+	RightSquareBracket
+	LeftCurlyBracket
+	RightCurlyBracket
+	Colon
+)
+
+var (
+	ErrNoValue error = errors.New("no value")
+)
+
+// TokenはValueTokenとMarkToken
+type Tokener interface {
+	TokenType() TokenType
+	Value() (any, error)
+}
+
+type ValueToken struct {
+	tokenType TokenType
+	value     any
+}
+
+func NewValueToken(tokenType TokenType, value any) ValueToken {
+	return ValueToken{
+		tokenType: tokenType,
+		value:     value,
+	}
+}
+
+func (vt ValueToken) TokenType() TokenType {
+	return vt.tokenType
+}
+
+func (vt ValueToken) Value() (any, error) {
+	return vt.value, nil
+}
+
+type MarkToken struct {
+	tokenType TokenType
+}
+
+func NewMarkToken(tokenType TokenType) MarkToken {
+	return MarkToken{tokenType: tokenType}
+}
+
+func (mt MarkToken) TokenType() TokenType {
+	return mt.tokenType
+}
+
+func (mt MarkToken) Value() (any, error) {
+	return nil, ErrNoValue
+}
+
+var (
+	ErrSyntax           error = errors.New("invalid syntax")
+	ErrUndefinedKeyword error = errors.New("undefined keyword")
+)
+
+func extractStringAsToken(str string, startIdx int) (ValueToken, int, error) {
+	// 直前に\がない"
+	// または 直前に\が偶数回連続している"
+	// `"`や`\\"`などがマッチ
+	re := regexp.MustCompile(`(?:^|[^\\]|((^|[^\\])(\\\\)+))(")`)
+	loc := re.FindStringSubmatchIndex(str[startIdx+1:])
+	if loc == nil {
+		return ValueToken{}, 0, ErrSyntax
+	}
+	// idxsはstr[firstQuotationIdx+1]からのインデックスであるためfirstQuotationIdx+1を足す
+	endIdx := startIdx + 1 + loc[1]
+
+	value, err := strconv.Unquote(str[startIdx:endIdx])
+	if err != nil {
+		return ValueToken{}, 0, ErrSyntax
+	}
+	token := ValueToken{
+		tokenType: String,
+		value:     value,
+	}
+	return token, endIdx, nil
+}
+
+func extractNumberAsToken(str string, startIdx int) (ValueToken, int, error) {
+	var token ValueToken
+	re := regexp.MustCompile(`\d+(\.\d+)?`)
+	loc := re.FindStringSubmatchIndex(str[startIdx:])
+	// 小数点以降のマッチはloc[2]からloc[3]
+	// loc[2] == -1 ならマッチしていないことになる
+	if loc[2] == -1 /*整数なら*/ {
+		value, err := strconv.Atoi(str[startIdx : startIdx+loc[1]])
+		if err != nil {
+			// errorを埋め込みたくないため%v
+			return ValueToken{}, 0, fmt.Errorf("internal error: %v", err)
+		}
+		token = ValueToken{
+			tokenType: Int,
+			value:     value,
+		}
+	} else /*floatなら*/ {
+		value, err := strconv.ParseFloat(str[startIdx:startIdx+loc[1]], 64)
+		if err != nil {
+			// errorを埋め込みたくないため%v
+			return ValueToken{}, 0, fmt.Errorf("internal error: %v", err)
+		}
+		token = ValueToken{
+			tokenType: Float,
+			value:     value,
+		}
+	}
+	return token, startIdx + loc[1], nil
+}
+
+func extractBoolAsToken(str string, startIdx int) (ValueToken, int, error) {
+	// ?i: はcase insentive
+	// ?: はグループをキャプチャしない
+	re := regexp.MustCompile(`(?i:true|false)(?:[\s,:"{}\[\]]|$)`)
+	loc := re.FindStringSubmatchIndex(str[startIdx:])
+	if loc == nil {
+		return ValueToken{}, 0, ErrUndefinedKeyword
+	}
+	endIdx := startIdx + loc[1]
+
+	var value bool
+	if str[startIdx:endIdx] == "true" {
+		value = true
+	} else {
+		value = false
+	}
+
+	token := ValueToken{
+		tokenType: Bool,
+		value:     value,
+	}
+	return token, endIdx, nil
+}
+
+func Analyze(d []byte) ([]Tokener, error) {
+	res := []Tokener{}
+
+	inputStr := string(d)
+	for i := 0; i < len(inputStr); i++ {
+		switch inputStr[i] {
+		case '"':
+			token, endIdx, err := extractStringAsToken(inputStr, i)
+			if err != nil {
+				return nil, err
+			}
+			res = append(res, token)
+			i = endIdx - 1
+		case '1', '2', '3', '4', '5', '6', '7', '8', '9', '0':
+			token, endIdx, err := extractNumberAsToken(inputStr, i)
+			if err != nil {
+				return nil, err
+			}
+			res = append(res, token)
+			i = endIdx - 1
+		case 't', 'f':
+			token, endIdx, err := extractBoolAsToken(inputStr, i)
+			if err != nil {
+				return nil, err
+			}
+			res = append(res, token)
+			i = endIdx - 1
+		}
+	}
+
+	return res, nil
+}
---
+++ parser/analyzer_test.go
@@ -0,0 +1,173 @@
+package parser_test
+
+import (
+	"errors"
+	"reflect"
+	"testing"
+
+	"github.com/YO-RO/mini-parser-go/parser"
+)
+
+type testCase struct {
+	name      string
+	input     string
+	expectErr error
+	want      []parser.Tokener
+}
+
+func runTestCases(t *testing.T, tests []testCase) {
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			got, err := parser.Analyze([]byte(tc.input))
+			if !errors.Is(tc.expectErr, err) {
+				t.Errorf("err(%v) expects to be %v", err, tc.expectErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tc.want) {
+				t.Errorf("parser.Analyze([]byte(%q)) == %v, want %v", tc.input, got, tc.want)
+			}
+		})
+	}
+
+}
+
+func intToken(t *testing.T, value int) parser.ValueToken {
+	t.Helper()
+	return parser.NewValueToken(parser.Int, value)
+}
+
+func floatToken(t *testing.T, value float64) parser.ValueToken {
+	t.Helper()
+	return parser.NewValueToken(parser.Float, value)
+}
+
+func strToken(t *testing.T, value string) parser.ValueToken {
+	t.Helper()
+	return parser.NewValueToken(parser.String, value)
+}
+
+func boolToken(t *testing.T, value bool) parser.ValueToken {
+	t.Helper()
+	return parser.NewValueToken(parser.Bool, value)
+}
+
+func TestBoolAnalyzer(t *testing.T) {
+	tests := []testCase{
+		{
+			"真",
+			"true",
+			nil,
+			[]parser.Tokener{boolToken(t, true)},
+		},
+		{
+			"偽",
+			"false",
+			nil,
+			[]parser.Tokener{boolToken(t, false)},
+		},
+		{
+			"定義されていないキーワード[trueeeee]",
+			"trueeeee",
+			parser.ErrUndefinedKeyword,
+			nil,
+		},
+	}
+	runTestCases(t, tests)
+}
+
+func TestIntAnalyzer(t *testing.T) {
+	tests := []testCase{
+		{
+			"一つの整数",
+			"123",
+			nil,
+			[]parser.Tokener{intToken(t, 123)},
+		},
+		{
+			"複数の整数",
+			"123 456 789",
+			nil,
+			[]parser.Tokener{
+				intToken(t, 123),
+				intToken(t, 456),
+				intToken(t, 789),
+			},
+		},
+	}
+	runTestCases(t, tests)
+}
+
+func TestFloatAnalyzer(t *testing.T) {
+	tests := []testCase{
+		{
+			"一つのfloat",
+			"12.34",
+			nil,
+			[]parser.Tokener{floatToken(t, 12.34)},
+		},
+		{
+			"複数のfloat",
+			"12.34 56.78 90.12",
+			nil,
+			[]parser.Tokener{
+				floatToken(t, 12.34),
+				floatToken(t, 56.78),
+				floatToken(t, 90.12),
+			},
+		},
+	}
+	runTestCases(t, tests)
+}
+
+func TestStringAnalyzer(t *testing.T) {
+	tests := []testCase{
+		{
+			"空文字列",
+			`""`,
+			nil,
+			[]parser.Tokener{
+				strToken(t, ""),
+			},
+		},
+		{
+			"文字列",
+			`"string"`,
+			nil,
+			[]parser.Tokener{
+				strToken(t, "string"),
+			},
+		},
+		{
+			"エスケープ文字付き文字列",
+			`"\n, \\, \", \\n, \\\", \\"`,
+			nil,
+			[]parser.Tokener{
+				strToken(t, "\n, \\, \", \\n, \\\", \\"),
+			},
+		},
+		{
+			"複数の文字列",
+			`"hello""world" "foo"`,
+			nil,
+			[]parser.Tokener{
+				strToken(t, "hello"),
+				strToken(t, "world"),
+				strToken(t, "foo"),
+			},
+		},
+		{
+			"不正な制御文字",
+			`"line1
+			line2"`,
+			parser.ErrSyntax,
+			nil,
+		},
+		{
+			"文字列を閉じるための\"がない",
+			`"string`,
+			parser.ErrSyntax,
+			nil,
+		},
+	}
+	runTestCases(t, tests)
+}
