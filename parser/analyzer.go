b31f17cc9c01b828ef974ef413c5c330235f4290
YO-RO <45329703+YO-RO@users.noreply.github.com>
2024-07-08 00:16:38 +0900

    fix: #7 と同じ間違いをしていた部分を修正
    85行目: グループをキャプチャしない場合は各グループに対して `?:` を書く必要がある。
            一部抜けていた箇所に追加した。
    
    91行目: loc[1]とloc[3]は同じ値なので #7 のような問題は起きない。
            しかし `"` の end index であるためloc[3]を使う方が適切だと判断した。

--- parser/analyzer.go
+++ parser/analyzer.go
@@ -82,13 +82,13 @@ func extractStringAsToken(str string, startIdx int) (ValueToken, int, error) {
 	// 直前に\がない"
 	// または 直前に\が偶数回連続している"
 	// `"`や`\\"`などがマッチ
-	re := regexp.MustCompile(`(?:^|[^\\]|((^|[^\\])(\\\\)+))(")`)
+	re := regexp.MustCompile(`(?:^|[^\\]|(?:(?:^|[^\\])(?:\\\\)+))(")`)
 	loc := re.FindStringSubmatchIndex(str[startIdx+1:])
 	if loc == nil {
 		return ValueToken{}, 0, ErrSyntax
 	}
 	// idxsはstr[firstQuotationIdx+1]からのインデックスであるためfirstQuotationIdx+1を足す
-	endIdx := startIdx + 1 + loc[1]
+	endIdx := startIdx + 1 + loc[3]
 
 	value, err := strconv.Unquote(str[startIdx:endIdx])
 	if err != nil {
